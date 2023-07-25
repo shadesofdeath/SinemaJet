c9c281a46bdf35e3db3db1581abca64c9e540516
Berkay AY <61064047+shadesofdeath@users.noreply.github.com>
2023-07-26 00:19:39 +0300

    :+1:

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const filmContainer = document.getElementById("filmContainer");
 const categorySelect = document.getElementById("categorySelect");
 const searchInput = document.querySelector('input[type="text"]');
 const filmCountElement = document.getElementById("filmCount");
+const loadMoreBtn = document.getElementById("loadMoreBtn");
 
 let films = [];
 let totalFilmCount = 0;
+let filmsPerPage = 20;
+let currentFilmIndex = 0;
+let selectedCategory = "all";
 
 function createFilmCard(film) {
   const card = document.createElement("div");
@@ -47,15 +51,22 @@ function createFilmCard(film) {
   filmContainer.appendChild(card);
 }
 
+
 function fetchFilms() {
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       if (xhr.status === 200) {
         films = JSON.parse(xhr.responseText);
-        totalFilmCount = films.length; // Update the totalFilmCount based on the number of films fetched
-        filmCountElement.textContent = totalFilmCount; // Display the film count on the webpage
-        films.forEach(createFilmCard);
+
+        // Apply category filter
+        if (selectedCategory !== "all") {
+          films = films.filter((film) => film.categories.includes(selectedCategory));
+        }
+
+        totalFilmCount = films.length;
+        filmCountElement.textContent = totalFilmCount;
+        loadMoreFilms();
       } else {
         console.error("Failed to fetch films.");
       }
@@ -65,35 +76,54 @@ function fetchFilms() {
   xhr.send();
 }
 
-function filterCards() {
-  const selectedCategory = categorySelect.value;
-  const cards = document.querySelectorAll(".card");
 
-  // Get the search query from the input field
+function filterCards() {
   const searchQuery = searchInput.value.trim().toLowerCase();
 
-  cards.forEach((card) => {
-    const filmTitle = card.querySelector("h2").textContent.toLowerCase();
-    const filmCategories = JSON.parse(card.getAttribute("data-categories"));
-    const titleMatch = filmTitle.includes(searchQuery);
-    const categoryMatch =
-      selectedCategory === "all" || filmCategories.includes(selectedCategory);
+  const filteredFilms = films.filter((film) => {
+    const filmTitle = film.title.toLowerCase();
+    const filmCategories = film.categories.map((category) => category.toLowerCase());
 
-    if (titleMatch && categoryMatch) {
-      card.style.display = "block";
-    } else {
-      card.style.display = "none";
-    }
+    const titleMatch = filmTitle.includes(searchQuery);
+    const categoryMatch = selectedCategory === "all" || filmCategories.includes(selectedCategory.toLowerCase());
+
+    return titleMatch && categoryMatch;
   });
+
+  // Clear the current film container
+  filmContainer.innerHTML = "";
+
+  // Create cards for the filtered films
+  filteredFilms.forEach(createFilmCard);
 }
+
+function loadMoreFilms() {
+  const filmsToShow = films.slice(currentFilmIndex, currentFilmIndex + filmsPerPage);
+  filmsToShow.forEach(createFilmCard);
+  currentFilmIndex += filmsPerPage;
+
+  // Hide the "Load More" button if all films are displayed
+  if (currentFilmIndex >= totalFilmCount) {
+    loadMoreBtn.style.display = "none";
+  }
+}
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   fetchFilms();
 
-  categorySelect.addEventListener("change", filterCards);
+  categorySelect.addEventListener("change", () => {
+    selectedCategory = categorySelect.value; // Update the selected category
+    currentFilmIndex = 0; // Reset currentFilmIndex when the category changes
+    filmContainer.innerHTML = ""; // Clear existing cards when the category changes
+    fetchFilms(); // Fetch films for the selected category
+  });
+
   searchInput.addEventListener("input", filterCards);
 
-  // Başlangıçta tüm filmleri göstermek için
   categorySelect.value = "all";
   filterCards();
+
+  loadMoreBtn.addEventListener("click", loadMoreFilms);
 });
